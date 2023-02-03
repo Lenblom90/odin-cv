9d41a5530a212e9aba3259d72622e88d9b240a61
lblom <lenblom@gmail.com>
2023-02-03 10:52:40 +0200

    Make header editable.
    Header has click event to show input and keydown event for updating
    header text.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,45 @@ import '../styles/Header.css';
 export default class Header extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      name: props.name,
+      isEditable: false
+    }  
   }  
 
+  editText = () => {
+    console.log(this.state.isEditable);
+    this.setState(function(prevState, prevProps) {
+      return {
+        isEditable: !prevState.isEditable
+      }});
+  }
+
+  handleInput = (e) => {
+    if(e.key === "Enter"){
+      this.setState((prevState, prevProps) => {
+        return {
+          name: e.target.value,
+          isEditable: !prevState.isEditable
+        }
+      })
+    }
+  }
+
   render() {
-    return (
-      <div className="Header">
-        <h1 className="Header-text">{this.props.name}</h1>
-      </div>
-    );
+    if(!this.state.isEditable){
+      return (
+        <div className="Header">
+          <h1 onClick={this.editText} className="Header-text">{this.state.name}</h1>
+        </div>
+      );  
+    } else {
+      return (
+        <div className="Header">
+          <input type="text" placeholder={this.props.name} onKeyDown={this.handleInput} />
+        </div>
+      )
+    }
     }
 
 }
